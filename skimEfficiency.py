5938d0c33470f72e02fd7348630c3669e29df2cc
duarte <duarte@ifca.unican.es>
2010-05-15 01:16:46 +0200

    Added the possibility of skim output files (in case the job crashed and we can get the output file)
    Ignore-this: e922e384b14e5cc9fbb4f54e981962eb
    
    darcs-hash:20100514231646-e973b-940a721d2e42cfbd6dc26c556530516c5ed0864b.gz

--- skimEfficiency.py
+++ skimEfficiency.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-skimEfficiency: check the efficiency of the skimming for the differents paths
+skimEfficiency: check the efficiency of the skimming (and pre-selection) for the differents paths
 """
 
 def sumEff( stdoutFile, sumDict ):
 	"""
+	FALLLLLLLLLLLLLLAAAAAAAAAAA QUE CONYO PASSA?
 	"""
 	import re
 
@@ -23,9 +24,14 @@ findBlock( stdoutFile, sumDict ) --> second argument must be a dict, but is %s""
 			break
 		counter += 1
 
+	#regexp = re.compile( '\D*\s*\S\s*\S\s*(?P<RUN>\d*)\s*(?P<Passed>\d*)\s*(?P<Failed>\d*)\s*\d\s*(?P<NAME>.*)$' )
 	regexp = re.compile( '\D*\s*\S\s*\S\s*(?P<RUN>\d*)\s*(?P<Passed>\d*)\s*(?P<Failed>\d*)\s*\d\s*(?P<NAME>.*)$' )
 	for i in xrange(counterDOWN+2,counter-1):
-		name = regexp.search( lines[i] ).group( 'NAME' )
+		try:
+			name = regexp.search( lines[i] ).group( 'NAME' )
+		except AttributeError:
+			print i
+			print stdoutFile
 		try:
 			sumDict[name]['RUN'] += int(regexp.search( lines[i] ).group( 'RUN' )) 
 			sumDict[name]['Passed'] +=  int(regexp.search( lines[i] ).group( 'Passed' ))
@@ -48,7 +54,7 @@ def showResults( sumDict, format='' ):
 		pass
 	toShow = '%s %s %s %s %s\n' % (''.ljust(20),'Runned'.rjust(10),'Passed'.rjust(10),'Failed'.rjust(10),'eff'.rjust(10) )
 	toShow += '\n'
-	for filter,status in sumDict.iteritems():
+	for filter,status in sorted(sumDict.iteritems(),reverse=True):
 		toShow += """%s %10d %10d %10d %10f\n""" \
 				% (filter.ljust(20),status['RUN'],status['Passed'],status['Failed'],status['Passed']/float(status['RUN']))
 	print toShow
@@ -58,26 +64,38 @@ if __name__ == '__main__':
 	from optparse import OptionParser
 	
 	parser = OptionParser()
-#fname = 'trackerRecoMaterial_3_2_7.xml'
 	parser.set_defaults(incdir = '.')
         parser.add_option( '-d', '--dir', action='store', dest='incdir', help='Directory (absolute or relative) where are the files' )
+        parser.add_option( '-s', '--skip', action='store', dest='skip', help='Skip the jobs listed (list of integers)' )
 
         ( opt, args ) = parser.parse_args()
 	import os
-	allFiles = filter( lambda x: ( x.find('CMSSW_') != -1 and x.find('.stdout') != -1 ), os.listdir(opt.incdir) )
+	allFiles = filter( lambda x: ( x.find('CMSSW_') == 0 and x.find('.stdout') != -1 ), os.listdir(opt.incdir) )
 	if len(allFiles) == 0:
 		errorMessage =  'Directory %s doesn\'t contains the CMSSW_*.stdout, insert a directory with \'-d\' option' % opt.incdir
 		parser.error( errorMessage )
+	#skip if any
+	if opt.skip:
+		skip = set([ 'CMSSW_'+i+'.stdout' for i in opt.skip.split(',')])
+		allFiles = list( set(allFiles).difference(skip) )
 
-	print '='*50
+	print '='*60
 	print 'Reporting skimming efficiencies, processed datasets: ', len(allFiles)
+	if opt.skip:
+		skip = opt.skip.split(',')
+		message = '     (Skip the following jobs:'
+		for i in skip:
+			message += ' '+i
+		message += ')'
+		print message
+	print '='*60
 
 	effTable = {}
 	for stdoutFile in allFiles:
 		effTable = sumEff( os.path.join(opt.incdir,stdoutFile), effTable )
 
 	showResults( effTable )
-		
+	print '='*60
 		
 	
 	
