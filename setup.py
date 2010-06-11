5a776be53e33cf42ca1fc31a993f878bb66cda19
duarte <duarte@ifca.unican.es>
2010-06-11 20:02:01 +0200

    Move some utilities, rename some modules
    Ignore-this: 6fc49cd203ae2338da7bc3a7f051f5d3
    
    darcs-hash:20100611180201-e973b-50c80d24ce3e1b41dfc6f5c66bb4aca959ceeea8.gz

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 setup(name='pytnp',
-		version='0.3.1',
+		version='0.4.0',
 		description='Python for Tag And Probe utils',
 		author='Jordi Duarte Campderros',
 		author_email='Jordi.Duarte.Campderros@cern.ch',
 		url='http://devel.ifca.es/~duarte/tnp/dist',
-		py_modules=['pytnp','rootlogon'],
+		#py_modules=['pytnpclass','__init__'],
 		scripts=['effPlots.py'],
+		package_dir={'pytnp':''},
+		packages=['pytnp','pytnp.utils'],
 		)
 
