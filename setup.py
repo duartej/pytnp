a0d925d02124c94507c8cc102559e6157524579e
duarte <duarte@ifca.unican.es>
2010-06-18 18:24:10 +0200

    Added weightCreator script: Build the weights (to respect a variable, pt usually) between to different resonances
    Ignore-this: 88157d631083db87d00c67dcea8a4421
    
    darcs-hash:20100618162410-e973b-d26c4c5c2e745d4802b36d4a5efb1a9a8a111f3c.gz

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(name='pytnp',
-		version='0.4.1',
+		version='0.5.0',
 		description='Python for Tag And Probe utils',
 		author='Jordi Duarte Campderros',
 		author_email='Jordi.Duarte.Campderros@cern.ch',
 		url='http://devel.ifca.es/~duarte/tnp/dist',
 		#py_modules=['pytnpclass','__init__'],
-		scripts=['effPlots.py','prepsubJobs.sh'],#'weightCreator.py],
+		scripts=['effPlots.py','prepsubJobs.sh','weightCreator.py'],
 		package_dir={'pytnp':''},
 		packages=['pytnp','pytnp.utils'],
 		)
