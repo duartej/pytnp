e3a52aecdd251846cccbe14b919a44d4534f834f
duarte <duarte@ifca.unican.es>
2010-08-08 13:09:24 +0200

    Added the copyFile and CowboysNTuple, modified setup to release 0.6.0
    darcs-hash:20100808110924-e973b-b904038f339f2626d3fbf8571334c0a72aa2c805.gz

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+from subprocess import Popen, PIPE
+
+
+# Generating the c++ executables  ---------------------
+# FIXME: Provisional code--> missing checks, error
+#        control, ...
+import os
+import shutil
+print '\033[1;39mGenerating C++ executables...\033[1;m'
+oldPath = os.getcwd()
+os.chdir('procsfile')
+_sources = os.listdir('.')
+try:
+	os.mkdir('.build')
+	for _file in _sources:
+		shutil.copy(_file,'./.build/'+_file)
+except OSError:
+	_sources.remove('.build')
+os.chdir('.build')
+ccode = Popen( ['make'],stdout=PIPE).communicate()[0]
+print ccode
+os.chdir( oldPath )
+#-------------------------------------------------------
+
+
 setup(name='pytnp',
-		version='0.5.1_patch2',
+		version='0.6.0',
 		description='Python for Tag And Probe utils',
 		author='Jordi Duarte Campderros',
 		author_email='Jordi.Duarte.Campderros@cern.ch',
 		url='http://devel.ifca.es/~duarte/tnp/dist',
 		#py_modules=['pytnpclass','__init__'],
-		scripts=['effPlots.py','prepsubJobs.sh','weightCreator.py'],
+		scripts=['effPlots.py','prepsubJobs.sh','weightCreator.py', 'procsfile/.build/copyFile', 'procsfile/.build/CowboysNTuple'],
 		package_dir={'pytnp':''},
 		packages=['pytnp','pytnp.utils'],
 		)
