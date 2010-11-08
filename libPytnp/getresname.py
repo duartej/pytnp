b9f2e0a37f71e0f014d18549a1b9fa428582bbf3
duarte <duarte@ifca.unican.es>
2010-11-08 15:09:19 +0100

    Implemented the inclusion of resonance name by a configuration file
    Ignore-this: f633e90d402a6cbe740b1ea6b5b43131
    
    darcs-hash:20101108140919-e973b-f1471230c0616cd96c291ab9f36a8e91c5fa9e9c.gz

--- libPytnp/getresname.py
+++ libPytnp/getresname.py
@@ -1,9 +1,11 @@
 """
 """
+from management import printError, printWarning, parserConfig
 
-def getResName( aFile ):
+
+def getResName( aFile, **keywords ):
 	"""
-	getResName( fileName ) -> str,str (latex)
+	getResName( fileName[, 'config=config_file.py'] ) -> str,str (latex)
 
 	Extract from file name, T&P-like, the resonance
 	and returns it plain and in Latex format.
@@ -29,6 +31,14 @@ def getResName( aFile ):
 			'_MC_' : (' MC', ''),
 			'_Spring10_' : (' MC', ''),
 			}
+	#-- Add and/or updating the keys, entered by the user
+	for key, _file in keywords.iteritems():
+		if key != 'config':
+			message = "Invalid argument key '%s', only accepted 'config" % key
+			printError( getResName.__module__+'.'+getResName.__name__, message, KeyError )
+		for name, value in parserConfig( keywords['config'], 'DataNames' ).iteritems():
+			nameDict[name] = value
+
 	try:
 		num = regexp.search( aFile ).group( 'NUMBER' )
 		resonanceLatex = '#Upsilon('+num+')'
@@ -41,21 +51,17 @@ def getResName( aFile ):
 				resonanceLatex = resLatex
 				resonance = res
 		if resonance == '':
-			message ="""\033[1;33mgetResName: 
-		WARNING: This function is highly dependent of the 
-		name of the file, you need an standard format like:
-		NameOFResonance_X_blabla.root
-
-		Unrecognized name: \033[1;m\033[1;31m  %s\033[1;m""" % aFile
-			print message
+			message ="""This function is highly dependent of the \n"""\
+					"""name of the file, you need an standard format like:\n"""\
+					"""    NameOFResonance_X_blabla.root"""\
+					"""Unrecognized name: \033[1;m\033[1;39m  '%s'\033[1;m""" % aFile
+			printWarning( getResName.__module__+'.'+getResName.__name__, message )
 			return None
 		
 	except:
-		message ="""\033[1;31mgetResName: 
-		ERROR: Something wrong!! Check the code pytnp.utils.getResName
-		because this is an unexpected error\033[1;m"""
-		print message
-		exit(-1)
+		message ="""UNEXPECTED ERROR!! Send a e-mail to the developer(s) with all information\n"""\
+				"""needed to reproduce this error"""
+		printError( getresname.__module__+'.'+getresname.__name__, message, RuntimeError )
 	
 	#Including others..
 	for name, (resLatex,res) in sorted(adjectDict.iteritems(),reverse=True):
--- libPytnp/management.py
+++ libPytnp/management.py
@@ -21,3 +21,55 @@ def printWarning( name, message ):
 	mess = mess[:-1]
 	print mess
 
+def parserConfig( config_file, key ):
+	"""
+	"""
+	import os.path
+	import sys
+
+	#-- Check the file is in the working directory
+	if not os.path.isfile( config_file ):
+		message = "File not exists: '%s'" % config_file
+		printError( parserConfig.__module__+'.'+parserConfig.__name__, message, IOError )
+
+	# FIXME: Potential errors with some python modules names
+	#        maybe the file name must be a unique hardcoded name
+	config_mod = config_file.replace('.py','')
+	sys.path.append( '.' )
+	try:
+		config = __import__(config_mod)
+	except ImportError:
+		print "You have to put the configuration file '%s' in the working directory" % config_file
+	#Dictionary of files --> (LatexName, name)
+	try:
+		value = eval('config.'+key)
+	except AttributeError:
+		message = "'%s' dictionary not found in the config file '%s'" % (key,config_file)
+		printError( parserConfig.__module__+'.'+parserConfig.__name__, message, AttributeError )
+	
+	if key == 'DataNames':
+		return parserDataNames(value)
+	else:
+		message = "Option '%s' not understood for the configuration" % key
+		printError( parserConfig.__module__+'.'+parserConfig.__name__, message, AttributeError )
+
+
+def parserDataNames(DataName):
+	"""
+	"""
+	#-- Check the right format:
+	isFormatRight = True
+	namesDict = {}
+	for fileName, value in DataName.iteritems():
+		if not isinstance(value, tuple):
+			message = "Erroneous format:\n\t\t"
+			message += "%s: %s" % (fileName, str(value))
+			printWarning( parserConfig.__module__+'.'+parserConfig.__name__, message )
+			isFormatRight = False
+		namesDict[fileName] = (value[0],value[1])
+	
+	if not isFormatRight:
+		message = "Error parsing '%s'" % config_file
+		printError( parserConfig.__module__+'.'+parserConfig.__name__, message, AttributeError )
+
+	return namesDict
